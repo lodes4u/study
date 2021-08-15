f1978c2cfd5f1f5e2a9bdc4ecb6dd75815543ffc
KwonHyungLee <lodes4u@gmail.com>
2021-08-15 22:44:09 +0900

    initial release

---
+++ src/main/java/me/myds/lodes4u/study/StudyApplication.java
@@ -0,0 +1,13 @@
+package me.myds.lodes4u.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+	// 테스트입니다.
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplication.class, args);
+	}
+
+}
